49ce2a1ad2dc9d4a85b71d74e9105c4d8292f6e8
Clause828 <55769079+Clause828@users.noreply.github.com>
2019-12-03 00:45:23 -0800

    header comments for all .h files

--- HashMapDatabase.h
+++ HashMapDatabase.h
@@ -1,4 +1,15 @@
-//HashMapDatabase (aka Customers database)
+// ------------------------------------------------ HashMapDatabase.h --------------------------------------------------------
+// Jayden Stipek Nihal CSS 343 B
+// Creation: 10/25/19
+// Modified: 12/2/19
+// -----------------------------------------------------------------------------------------------------------------
+// Purpose - Encapsulate customer data
+// -----------------------------------------------------------------------------------------------------------------
+// Notes:
+//		Uses a hashmap implemented as a vector
+//		Hash perfect hashing due to their being unique ID's
+//		Able to add customers, remove, and other information as needed
+// -----------------------------------------------------------------------------------------------------------------
 
 #pragma once
 #include "Customers.h"
--- Transactions.h
+++ Transactions.h
@@ -1,5 +1,17 @@
-// Transaction and TransactionDatabase classes
-
+// ------------------------------------------------ Transactions.h--------------------------------------------------------
+// Jayden Stipek Nihal CSS 343 B
+// Creation: 11/20/19
+// Modified: 12/2/19
+// -----------------------------------------------------------------------------------------------------------------
+// Purpose - The Driver for the Store (Reads in files and executes commands)
+// -----------------------------------------------------------------------------------------------------------------
+// Notes:
+//		Has a vector for all types of Transaction
+//		Genres include (Inventory(I), History(H),Return(R), and Borrow(B))
+//		Reads in files and creates movie, customer, and transaction obbjects
+//		Afterwards executes commands for each Transaction created
+// 
+// -----------------------------------------------------------------------------------------------------------------
 #pragma once
 #include <iostream>
 #include <queue>
